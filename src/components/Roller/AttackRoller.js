465c689bcfa3bdd2dfcf99033f82f8b4abc36a06
tstrat <tstrat@email.arizona.edu>
2018-11-14 11:42:44 -0700

    Fixed case where description wasnt working and rolls of prev were being used

--- src/components/Roller/AttackRoller.js
+++ src/components/Roller/AttackRoller.js
@@ -16,7 +16,6 @@ export default class AttackRoller extends Component {
         const { actions } = this.props;
         const { rolls } = this.state;
         for (let key in actions) {
-            console.log('MonsterType:', key);
             const monsterType = this.getMonsterType(key);
             for( let i = 0; i < actions[key].count; i++) {
                 let toHit, damage;
@@ -27,7 +26,6 @@ export default class AttackRoller extends Component {
                     let r = splitDiceRollString(actions[key].damage_dice).map(n => parseInt(n));
                     // rolls[key][damage] = dieRoller()
                     damage = dieRoller(...r, actions[key].damage_bonus || 0);
-                    console.log(damage);
                 }
 
                 const newRoll = { 'to_hit': toHit || null, damage, desc: actions[key].desc, name: this.getAttackType(key) };
@@ -59,23 +57,22 @@ export default class AttackRoller extends Component {
         if (!Object.keys(rolls).length) {
             return <div>Loading..</div>
         }
-        console.log( rolls);
         const rollDisplay = [];
         for ( let key in rolls ) {
-            let toHit = null, damage = null;
+            
             const next = rolls[key];
-            console.log('NEXT:', next);
             
             const innerDisplay = [];
             next.forEach((roll, i) => {
-                console.log(roll);
-                console.log(`KEY: ${key}, ROLL: ${roll.to_hit}`);
+                let toHit = null, damage = null;
                 if (roll.to_hit) {
                     toHit = <p>To-Hit: ( { roll.to_hit[0][0] } ) + { roll.to_hit[1] } = { roll.to_hit[2][0] }</p>;
                 }
                 if (roll.damage) {
                     damage = <p>Damage: ( { roll.damage[0].join(', ') } ) + { roll.damage[1] } = { roll.damage[2] }</p>
                 }
+            
+                /* TO-DO: STYLING */
                 innerDisplay.push(
                     <div key={roll+i} className='roll'>
                         <h3>{roll.name}</h3>
@@ -92,8 +89,6 @@ export default class AttackRoller extends Component {
                 </div>
             );
         }
-
-        console.log('Roll display: ',rollDisplay);
         
         return (
             <div className='attack-rolls'>
